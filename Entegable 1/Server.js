3d5b3d11e8bcd9c9c30b47af10d2a8dcf50aace5
joaquin-okunz <121913751+joaquin-okunz@users.noreply.github.com>
2023-02-14 22:28:18 -0300

    Add files via upload

--- Entegable 1/Server.js
+++ Entegable 1/Server.js
@@ -21,6 +21,7 @@ routerProductos.use(bodyParser.urlencoded({ extended: true }));
 routerCarrito.use(bodyParser.urlencoded({ extended: true }));
 
 //---------------------------------------------------------------------------
+//Configuramos multer
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, './upload')
@@ -41,35 +42,37 @@ app.post('/subir', uploads.single('miArchivo'), (req, res, next) => {
     else res.send(`!Archivo subido exitosamente!`);
 })
 //---------------------------------------------------------------------------
+//Listar todos los productos
 routerProductos.get("/listar", (req, res) => {
     res.json(prods.getProducts())
 })
-
+//Listar los productos por id
 routerProductos.get("/listar/:pid", (req, res) => {
     let { pid } = req.params;
     res.json(prods.getProductById(pid));
 })
 
-
+//Listar todos los productos hasta sierto limite
 routerProductos.get("/limit/:pid", (req, res) => {
     let { pid } = req.params;
     res.json(prods.getProductsWhithLimit(pid));
 })
 
-
+//Guardar productos
 routerProductos.post("/guardar", (req, res) => {
     let producto = req.body;
     prods.addproduct(producto);
     res.json(producto);
 })
 
+//Borrar productos
 routerProductos.delete("/borrar/:pid", (req, res) => {
     let { pid } = req.params;
     let producto = prods.deleteProducts(pid);
     res.json(producto);
 })
 
-
+//Actualizar productos
 routerProductos.put("/actualizar/:pid", (req, res) => {
     let { pid } = req.params;
     let prod = req.body;
@@ -80,15 +83,18 @@ routerProductos.put("/actualizar/:pid", (req, res) => {
 
 //---------------------------------------------------------------------------
 
+//Listar carrito por id
 routerCarrito.get("/listar/:cid", (req, res) => {
     let { cid } = req.params;
     res.json(Carritos.getCartById(cid));
 })
 
+//Crear carrito
 routerCarrito.post("/crear", (req, res) => {
     res.json(Carritos.addCart());
 })
 
+//Agregar un producto especifico dentro de un carrito especifico.
 routerCarrito.post("/:cid/productos/:pid", (req, res) => {
     let  cid  = req.params.cid;
     let  pid  = req.params.pid;
@@ -102,22 +108,3 @@ const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 })
 server.on("error", error => console.log(`Error en servidor ${error}`))
-
-
-
-
-/*
-<h2> Actualice un producto</h2>
-        <form action="/productos/actualizar" method="PUT"> 
-            id:<input type="number" name="id">
-            Nombre: <input type="text" name="title">
-            descripcion: <input type="text" name="description ">
-            code: <input type="text" name ="code">
-            Precio: <input type="number" name="price">
-            urlImagen: <input type="text" name="thumbnail">
-            stock:<input type="number" name="stock">
-            status<input  type="boolean" name="status" value="true">
-            category:<input type="text" name="category">
-            <button>Enviar</button>
-        </form>  
-        */
