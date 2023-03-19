4bc70ae1a4fc54b85fc39b53323d7b32d850541e
joaquin-okunz <121913751+joaquin-okunz@users.noreply.github.com>
2023-03-18 21:00:29 -0300

    Add files via upload

--- Websocket/app.js
+++ Websocket/app.js
@@ -23,10 +23,13 @@ app.use(express.static(_Dirname + "/public"));
 app.use(express.static(_Dirname + "/public/layouts"));
 
 //--------------------------------------------------------------------------------------
-
 function getProd () {
     let productos = prods.getProducts();
     return productos;
+}
+
+function actualizar () {
+
 }
 
 
@@ -47,10 +50,12 @@ app.get("/", (req,res) => {
     res.render("index", {productos});
 })
 
+//--------------------------------------------------------------------------------------
 app.get("/realTime", (req,res) => {
     let productos = prods.getProducts();
-    const script = { socket: '/socket.io/socket.io.js', index: 'index.js', productos : productos}
+    const script = { socket: '/socket.io/socket.io.js', index: '/js/index.js', prod : productos}
     res.render("realTimeProducts", script);
+
 })
 
 io.on('connection', (clienteSocket) => {
@@ -59,11 +64,7 @@ io.on('connection', (clienteSocket) => {
 
     clienteSocket.emit('inicio', getProd() );
 
-    clienteSocket.on('lista-producto', (data) => {
-        console.log('data: ' + data);
-    })
-
-
+    
     clienteSocket.on("desconection", () => {
         console.log("cliente desconectado");
     });
