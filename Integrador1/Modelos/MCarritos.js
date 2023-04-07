11e20e62ce8eff4452deab424dba45852097001d
joaquin-okunz <121913751+joaquin-okunz@users.noreply.github.com>
2023-04-07 12:06:53 -0300

    Delete Integrador1 directory

--- Integrador1/Modelos/MCarritos.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-
-const Carr = new mongoose.Schema({
-  Productos: { type: Array, require: true }
-}, { timestamps: true })
-
-export default mongoose.model('Carritos', Carr)
